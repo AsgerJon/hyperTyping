8366d2fd302b185a014ca6c8eb0c27a37db17bc8
Asger Jon Vistisen <asgerjon2@gmail.com>
2023-08-12 12:47:41 +0200

    Fuck dyn types!

--- src/hypertyping/__init__.py
+++ src/hypertyping/__init__.py
@@ -1,1 +1,5 @@
-"""The hyperTyping module provides classes derived from the builtin typing module"""
+"""The hyperTyping module provides classes derived from the builtin typing
+module"""
+#  MIT Licence
+#  Copyright (c) 2023 Asger Jon Vistisen
+from __future__ import annotations
---
+++ src/hypertyping/_callmemaybe.py
@@ -0,0 +1,12 @@
+"""CallMeMaybe is a class that considers any callable object a member of
+itself."""
+#  MIT Licence
+#  Copyright (c) 2023 Asger Jon Vistisen
+from __future__ import annotations
+
+
+class CallMeMaybe:
+  """CallMeMaybe is a class that considers any callable object a member of
+  itself."""
+
+  returnType = Field()
